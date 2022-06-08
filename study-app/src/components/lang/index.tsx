8c33233f90f05690c23537064c72316fa92ffbf6
Anna <missjellx@gmail.com>
2022-06-08 21:03:44 +0300

    feat: add lang switcher

--- study-app/src/components/lang/index.tsx
+++ study-app/src/components/lang/index.tsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
+import i18n from '../../i18n';
 import './style.css';
 
+export enum LangType {
+  en = 'en',
+  ru = 'ru',
+}
+
 export const Lang = () => {
+  const [lang, toggleLang] = useState<string>(LangType.ru);
+
+  const changeLang = () => {
+    const currentLang = lang === LangType.ru ? LangType.en : LangType.ru;
+    toggleLang(currentLang);
+    i18n.changeLanguage(currentLang);
+  };
   return (
-    <div className='select-container'>
+    <div className="select-container">
       <img src="./assets/lang.png" alt="lang" className="select-img" />
-      <select className="select">
+      <select className="select" onChange={changeLang}>
         <option>RU</option>
         <option>EN</option>
       </select>
