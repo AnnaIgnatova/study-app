a61190ca8697032a3744c65b5de446b04c8bdd4c
Anna <missjellx@gmail.com>
2022-06-16 21:56:05 +0300

    feat: add theory page

--- study-app/src/components/filledBtn/index.tsx
+++ study-app/src/components/filledBtn/index.tsx
@@ -2,9 +2,16 @@ import './style.css';
 
 export interface BtnProps {
   text: string;
+  right?: boolean;
+  left?: boolean;
 }
 
 export const FilledBtn = (props: BtnProps) => {
-  const { text } = props;
-  return <button className="filled-btn">{text}</button>;
+  const { text, right, left } = props;
+  return (
+    <button className="filled-btn">
+      {text}
+      {right && <img src="./assets/arrow-right.png" alt="arrow right" />}
+    </button>
+  );
 };
--- study-app/src/pages/theory/index.tsx
+++ study-app/src/pages/theory/index.tsx
@@ -1,3 +1,45 @@
+import { FilledBtn } from '../../components/filledBtn';
+import { NavBlock } from '../main/NavBlock';
 import './style.css';
 
-export const Theory = () => <div className="theory">theory</div>;
+export const Theory = () => {
+  return (
+    <div className="main">
+      <NavBlock />
+      <div className="tasks-container theory-container">
+        <span className="theory-subtitle">Основы современной вёрстки</span>
+        <div className="title-level">
+          <h2 className="main-title theory-title">Базовая структура HTML документа</h2>
+          <div className="theory-level">
+            <span>сложность урока</span>
+            <div className="theory-level-num">2</div>
+          </div>
+        </div>
+        <hr className="theory-line" />
+        <div className="theory-description">
+          <p>
+            Как театр начинается с вешалки, так и любой HTML-документ начинается с базовой
+            структуры. Она включает в себя теги, которые есть в любом HTML-файле. Эти теги и
+            служебная информация нужны браузеру для корректного отображения информации.
+          </p>
+          <p>Взглянем на базовую структуру любого HTML-документа:</p>
+          <img src="./assets/theory-example.png" alt="example" />
+          <p>
+            Этот набор кажется не очень большим, но браузеру он сообщает множество полезной
+            информации. В этом уроке разберёмся с каждой строчкой этой структуры.
+          </p>
+          <p>DOCTYPE</p>
+          <p>
+            Первая конструкция в любом HTML-документе — элемент DOCTYPE. Он не относится к тегам и
+            никаким образом не может отображаться на странице. Его задача — указать браузеру, какой
+            стандарт HTML используется в этом документе. Сейчас это везде стандарт HTML5.
+            Записывается он следующим образом:
+          </p>
+        </div>
+        <div className="theory-btn-container">
+          <FilledBtn text="примеры заданий" right={true} />
+        </div>
+      </div>
+    </div>
+  );
+};
